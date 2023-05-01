449e1507d42d52a1bd8b306c6fcc22dc321f7cab
khyojun <nandong5923@naver.com>
2023-05-01 15:59:07 +0900

    feat: movieController 생성 , 폴더구조 수정

---
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/movie/MovieController.java
@@ -0,0 +1,25 @@
+package com.ItsTime.ItNovation.controller.movie;
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class MovieController {
+
+    @Autowired // new 로 생성해서 스프링 컨테이너에 등록되어있는 @Service 것을 들고와야 한다. DI 활용해야 다른 어노테이션들이 작동한다.   -> 오류 기록하기 나중에 !
+    TmdbApiClient tmdbApiClient; // autowired 해서 생성하지 않고 new 로 생성하면 @Value 어노테이션이 작동이 되지 않는다.
+
+    //getMovieAndPoster() 메서드를 호출할 때 TmdbApiClient 클래스의 인스턴스를 생성하지 않았거나, 생성한 인스턴스가 스프링 컨테이너에서 관리되지 않는 경우입니다.
+    //https://kimcoder.tistory.com/415?category=967734 출처
+    @GetMapping("/movie")
+    public String getMovieName(Model model) {
+        //TmdbApiClient tmdbApiClient1 = new TmdbApiClient(); // 이렇게 호출해서 사용하면 절대로 안에 있는 스프링 관련된 친구들이 작동하지를 않는다.
+
+        Map<String, String> movieAndPoster = tmdbApiClient.getMovieAndPoster();
+        System.out.println("hi");
+        model.addAttribute("movieInfo", movieAndPoster);
+        return "hi";
+    }
+
+}
