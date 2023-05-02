73c925e6426623746a59315bb5eb204d5ebb33f6
djc06048 <gsafe1213@gmail.com>
2023-05-02 12:10:08 +0900

    :art: fix: spring security 관련 설정 변경

--- ItNovation/src/main/java/com/ItsTime/ItNovation/config/auth/SecurityConfig.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/config/auth/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.ItsTime.ItNovation.config.auth;
 
+경import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
@@ -7,8 +10,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Configuration
@@ -26,9 +33,12 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .formLogin().disable()
+                .logout()
+                .logoutSuccessUrl("/home")
+                .and()
                 .httpBasic().disable()
                 .authorizeHttpRequests()
                     .requestMatchers("/", "/oauth2/**", "/login/**", "/css/**", "/images/**", "/js/**", "/console/**").permitAll()
