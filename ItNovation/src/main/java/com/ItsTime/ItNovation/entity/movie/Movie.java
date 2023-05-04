659e72381fef08e64a7c2e5cc12017c75ba55f42
khyojun <nandong5923@naver.com>
2023-05-04 14:54:06 +0900

    :sparkles: feat: 영화 엔티티 클래스 추가

---
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/entity/movie/Movie.java
@@ -0,0 +1,28 @@
+package com.ItsTime.ItNovation.entity.movie;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Movie {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long movieId;
+
+
+    private String movieTitle;
+
+    private String moviePosterUrl;
+
+
+
+}
