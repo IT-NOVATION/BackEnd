efe88be2d813db2b38460e619b35774d8a3dda23
khyojun <nandong5923@naver.com>
2023-05-17 20:40:04 +0900

    #17 feat: 기존의 Map을 통해 긁어오는 것이 아닌 JsonObject를 통하여서 parsing 후 긁어오는 방식으로 변경

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/movie/MovieCrawlService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/movie/MovieCrawlService.java
@@ -5,10 +5,14 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -34,12 +38,12 @@ public class MovieCrawlService {
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
         total_Pages(restTemplate);
-        Map<String, Movie> titleAndMovie = getTitleAndPoster(restTemplate);
+        Map<String, Movie> titleAndMovie = getTitleAndMovie(restTemplate);
 
         return titleAndMovie;
     }
 
-    public String findBgImg(){
+    public String findBgImg() {
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
@@ -48,72 +52,121 @@ public class MovieCrawlService {
     }
 
 
-
-
-    private Map<String, Movie> getTitleAndPoster(RestTemplate restTemplate) {  // 이 기능은 반드시 따로 빼서 스케줄러 돌려서 일정 주기마다 하기로 진행
+    private Map<String, Movie> getTitleAndMovie(
+        RestTemplate restTemplate) {  // 이 기능은 반드시 따로 빼서 스케줄러 돌려서 일정 주기마다 하기로 진행
         Map<String, Movie> titleAndMovie = new HashMap<>();
         for (int i = 1; i < 10; i++) {
             String url = "https://api.themoviedb.org/3/discover/movie" + "?api_key=" + API_KEY
                 + "&page=" + i + "&language=ko-KR";
-            Map<String, List<Map<String, Object>>> res1 = restTemplate.getForObject(url, Map.class); //여기에서도 끌고 올 수 있음. backdropPath 끌고 올 수 있음.
-            List<Map<String, Object>> results1 = res1.get("results");
-
-            for (Map<String, Object> movieInfoMap : results1) {
-
-                String title = (String)movieInfoMap.get("original_title");
-                String posterPath = (String)movieInfoMap.get("poster_path");
-                String backdropPath = (String)movieInfoMap.get("backdrop_path");
-                Integer movieId = (Integer)movieInfoMap.get("id");
-                String originalLanguage = (String)movieInfoMap.get("original_language");
-                String overView = (String)movieInfoMap.get("overView");
-                String releaseDate = (String)movieInfoMap.get("release_date");
-
+            ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
+//여기에서도 끌고 올 수 있음. backdropPath 끌고 올 수 있음.
+            String json = responseEntity.getBody();
+            JSONObject jsonObject = new JSONObject(json);
+            JSONArray results = jsonObject.getJSONArray("results");
+            for (int j=0; j<results.length(); j++ ) {
+                JSONObject movieObject = results.getJSONObject(i);
+                String title =  movieObject.getString("original_title");
+                String posterPath = movieObject.getString("poster_path");
+                String backdropPath = movieObject.getString("backdrop_path");
+                Integer movieId = movieObject.getInt("id");
+                String originalLanguage = movieObject.getString("original_language");
+                String overView = movieObject.getString("overview");
+                System.out.println(overView);
+                String releaseDate = movieObject.getString("release_date");
                 posterPath = "https://www.themoviedb.org/t/p/original/" + posterPath;
                 backdropPath = "https://www.themoviedb.org/t/p/original/" + backdropPath;
-
                 // 이제 긁어올것 장르, 배우, 감독, 나라, 러닝타임,
-
+                long real_movieId = movieId.longValue();
                 movieDetails(movieId);
                 System.out.println("========================");
-                movieCredit(movieId);
+                Map<String, String> actorAndDirector = movieCredit(movieId);
+                String director = actorAndDirector.get("Director");
+                String actor = actorAndDirector.get("Actor");
+                System.out.println("movie name & director = " + title + director);
+                System.out.println("movie name & actor = " +title + actor);
+                Map<String, String> runtimeAndGenre = movieDetails(movieId);
+                String genre = runtimeAndGenre.get("genre");
+                String runtime = runtimeAndGenre.get("runtime");
+                int runt = Integer.parseInt(runtime);
+                String country = runtimeAndGenre.get("country");
+                Movie movie = Movie.builder().
+                    title(title).
+                    movieImg(posterPath).
+                    movieBgImg(backdropPath).
+                    movieActor(actor).
+                    movieCountry(originalLanguage).
+                    movieDate(releaseDate).
+                    movieGenre(genre).
+                    movieDirector(director).
+                    movieRunningTime(runt).
+                    movieDetail(overView).
+                    real_movieId(real_movieId).build();
 
-                //[adult, backdrop_path, id, original_language, overview, popularity, release_date, title,  vote_average, vote_count]
+                titleAndMovie.put(title, movie);
             }
         }
+        System.out.println(titleAndMovie.toString());
         return titleAndMovie;
     }
 
-    private void movieCredit(Integer movieId) {
-        String movieUrl = String.format("https://api.themoviedb.org/3/movie/%d/credits?api_key=%s&language=ko-KR",
+    private Map<String, String> movieCredit(Integer movieId) {
+        String movieUrl = String.format(
+            "https://api.themoviedb.org/3/movie/%d/credits?api_key=%s&language=ko-KR",
             movieId, API_KEY);
         log.info(movieUrl);
         RestTemplate restTemplate2 = new RestTemplate();
-        Map<String, List<Map<String, Object>>> movieCredits= restTemplate2.getForObject(movieUrl, Map.class);
-        List<Map<String, Object>> cast = movieCredits.get("cast");
-        // 배우 테이블을 만들어야 할듯
-        System.out.println("movieDetail = " +  cast);
+        Map<String, String> actorAndDirector = new HashMap<>();
+        ResponseEntity<String> creditEntity = restTemplate2.getForEntity(movieUrl, String.class);
+        String json2 = creditEntity.getBody();
+        JSONObject jsonObject = new JSONObject(json2); //  Json으로 값들 바꾸고 영화 엔티티에 Actor, Pd 정보들 추가
+
+        //JSONObject credits = jsonObject.getJSONObject("credits");
+        JSONArray crew = jsonObject.getJSONArray("crew");
+        for (int i = 0; i < crew.length(); i++) {
+            JSONObject member = crew.getJSONObject(i);
+            if (member.getString("job").equals("Director")) {
+                actorAndDirector.put("Director", member.getString("name"));
+            }
+            if (member.getString("known_for_department").equals("Acting")) {
+                actorAndDirector.put("Actor", member.getString("name"));
+            }
+
+        }
+        return actorAndDirector;
     }
 
-    private void movieDetails(Integer movieId) {
-        String movieUrl = String.format("https://api.themoviedb.org/3/movie/%d?api_key=%s&language=ko-KR", movieId, API_KEY);
+    private Map<String, String> movieDetails(Integer movieId) {
+        String movieUrl = String.format(
+            "https://api.themoviedb.org/3/movie/%d?api_key=%s&language=ko-KR", movieId,
+            API_KEY);
         log.info(movieUrl);
-        RestTemplate restTemplate1 = new RestTemplate();
-        Map<String, List<Map<String, Object>>> movieDetail= restTemplate1.getForObject(movieUrl, Map.class);
-        System.out.println("movieDetail = " + movieDetail);
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> creditEntity = restTemplate.getForEntity(movieUrl, String.class);
+        String json2 = creditEntity.getBody();
+        JSONObject jsonObject = new JSONObject(json2); //  Json으로 값들 바꾸고 영화 엔티티에 Actor, Pd 정보들 추가
+        Map<String, String> runtimeAndGenre = new HashMap<>();
+        Integer runtime = jsonObject.getInt("runtime");
+        JSONArray genres = jsonObject.getJSONArray("genres");
+
+        JSONObject member = genres.getJSONObject(0);
+        String genre = member.getString("name");
+        runtimeAndGenre.put("runtime", runtime.toString());
+        runtimeAndGenre.put("genre", genre);
+        return runtimeAndGenre;
     }
 
-    private String getBgImgUrl(RestTemplate restTemplate){
+    private String getBgImgUrl(RestTemplate restTemplate) {
         String url = "https://api.themoviedb.org/3/movie/109445/images?api_key=" + API_KEY;
-        Map<String, List<Map<String,String>>> forObject = restTemplate.getForObject(url, Map.class);
+        Map<String, List<Map<String, String>>> forObject = restTemplate.getForObject(url,
+            Map.class);
         String fileUrl = forObject.get("backdrops").get(0).get("file_path");
         System.out.println("map " + forObject.get("backdrops").get(3).get("file_path"));
         return "https://www.themoviedb.org/t/p/original/" + fileUrl;
     }
 
-
-
     private void total_Pages(RestTemplate restTemplate) {
-        Map<String, List<Map<String, Object>>> res = restTemplate.getForObject(BASE_URL + API_KEY,
+        Map<String, List<Map<String, Object>>> res = restTemplate.getForObject(
+            BASE_URL + API_KEY,
             Map.class);
         Object totalPages = res.get("total_pages");
         log.info("totalpages= " + totalPages);
@@ -121,30 +174,3 @@ public class MovieCrawlService {
 
 }
 
-
-
-
-
-/* 이 코드 참고해서 위에 map같이 더럽게 한 거 수정해보기.
-import org.springframework.http.ResponseEntity;
-    import org.springframework.web.client.RestTemplate;
-    import org.json.JSONObject;
-
-public class TmdbDirectorFetcher {
-    public String fetchDirector(int movieId) {
-        RestTemplate restTemplate = new RestTemplate();
-        String url = "https://api.themoviedb.org/3/movie/" + movieId + "?api_key=<your-api-key>&language=en-US";
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity(url, String.class);
-        String json = responseEntity.getBody();
-        JSONObject jsonObject = new JSONObject(json);
-        JSONObject credits = jsonObject.getJSONObject("credits");
-        JSONArray crew = credits.getJSONArray("crew");
-        for (int i = 0; i < crew.length(); i++) {
-            JSONObject member = crew.getJSONObject(i);
-            if (member.getString("job").equals("Director")) {
-                return member.getString("name");
-            }
-        }
-        return null;
-    }
-}*/
