de780cbd9fcf3f597953b752396cc561b7cdfc34
[J-neat] <[junghyo0258@catholic.ac.kr]>
2023-05-22 21:27:43 +0900

    fix/ReviewMovie

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/movie/MovieCrawlService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/movie/MovieCrawlService.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
- -
+
 @Service
 @Slf4j
 public class MovieCrawlService {
