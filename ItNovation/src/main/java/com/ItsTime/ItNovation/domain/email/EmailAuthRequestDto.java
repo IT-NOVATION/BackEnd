0b545915b5b4a7f0221c27c5b635addc63f275bb
khyojun <nandong5923@naver.com>
2023-06-22 16:19:03 +0900

    #25 feat: mail 인증 관련 DTO 추가

---
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/email/EmailAuthRequestDto.java
@@ -0,0 +1,15 @@
+package com.ItsTime.ItNovation.domain.email;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+public class EmailAuthRequestDto {
+
+
+    private String email;
+
+
+}
