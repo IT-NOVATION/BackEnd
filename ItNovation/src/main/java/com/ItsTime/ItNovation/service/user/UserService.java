57da49294b153cfdc00bca5ca4bad5a7fe8fa1c4
djc06048 <gsafe1213@gmail.com>
2023-06-26 17:19:07 +0900

    [fix] userService join 엔드포인트 API 규격 맞춰 수정
    성공시 200, 실패시 401 에러 나게 수정

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/user/UserService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/user/UserService.java
@@ -22,7 +22,6 @@ import java.util.Optional;
 @RequiredArgsConstructor //final 붙은 필드에 대해 생성자 생성
 @Slf4j
 @Service
-
 public class UserService {
 
     private final UserRepository userRepository;
@@ -30,29 +29,30 @@ public class UserService {
 
 
     @Transactional
-    public ResponseEntity<SignUpResponseDto> join(SignUpRequestDto signUpRequestDto) {
+    public ResponseEntity<?> join(SignUpRequestDto signUpRequestDto) {
+        log.info("회원가입");
         String result = validateDuplicateUser(signUpRequestDto);
-        SignUpResponseDto signUpResponseDto=SignUpResponseDto.builder().userId(null).build();
         log.info(result);
         if (StringUtils.hasText(result)) {
 
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                    .body(signUpResponseDto); //wrapper 타입만 null 이 들어갈 수 있음
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+                     //wrapper 타입만 null 이 들어갈 수 있음
         } else {
             User user = User.builder()
                     .email(signUpRequestDto.getEmail())
                     .password(signUpRequestDto.getPassword())
-                    .role(Role.GUEST) //TODO: 두번째 로그인 진행해야함. 소개글과 별명 입력하면 Role.GUEST로 권한 변경됨
+                    .role(Role.GUEST)
                     .build();
 
             user.passwordEncode(passwordEncoder);
 
             userRepository.save(user);
             log.info(user.getEmail() + " 회원가입 성공");
-            return ResponseEntity.ok(SignUpResponseDto.builder().userId(user.getId()).build());
+            return ResponseEntity.ok(null);
         }
 
     }
+
 
     private String validateDuplicateUser(SignUpRequestDto signUpRequestDto) {
         Optional<User> findEmail = userRepository.findByEmail(signUpRequestDto.getEmail());
@@ -64,4 +64,6 @@ public class UserService {
         return errorMessage;
 
     }
+
+
 }
