2d813b1c2531aa70e084f3da550cd5ffbd3a87a7
khyojun <nandong5923@naver.com>
2023-06-27 17:21:14 +0900

    fixed untracked filed

--- ItNovation/src/test/java/com/ItsTime/ItNovation/experiment/SaveTest.java
+++ ItNovation/src/test/java/com/ItsTime/ItNovation/experiment/SaveTest.java
@@ -27,12 +27,6 @@ public class SaveTest {
     @Autowired
     private MovieRepository movieRepository;
 
-
-
-
-
-
-
     @Test
     @Transactional
     public void 같은_트랜잭션에서_같은객체인지(){
@@ -43,14 +37,10 @@ public class SaveTest {
             .movieDetail("fwefsv").movieDate("1234").movieActor("12345")
             .title("helloworld").movieImg("1234").movieBgImg("12345").real_movieId(444332L).build();
 
-
         //when
-        Map<String, Movie> movieInfo = new HashMap<>();
-        movieInfo.put("helloworld", originMovie);
-        repoService.saveMovie(movieInfo);
-        Optional<Movie> helloworld = repoService.findByTitle("helloworld");
+        movieRepository.save(originMovie);
+        Optional<Movie> helloworld = movieRepository.findByTitle("helloworld");
         Movie findMovie = helloworld.get();
-
         //then
         Assertions.assertThat(originMovie).isEqualTo(findMovie); // 같은 영속
 
@@ -58,27 +48,38 @@ public class SaveTest {
 
 
     @Test
-    @Transactional
     @Rollback(value = false)
+    @Transactional
+    public void 미리DB에저장(){
+        Movie originMovie = Movie.builder().
+            movieCountry("en").movieGenre("12").movieRunningTime(123).movieDirector("123")
+            .movieDetail("fwefsv").movieDate("1234").movieActor("12345")
+            .title("helloworld2").movieImg("1234").movieBgImg("12345").real_movieId(444332L).build();
+
+        Map<String, Movie> titleAndMovie= new HashMap<>();
+        titleAndMovie.put("helloworld2", originMovie);
+        repoService.saveMovie(titleAndMovie); // 기존에 있는 경우 다시 넣지 않음.
+    }
+
+
+    @Test
+    @Transactional
     public void 기존db에서불러왔을때_equal하게보는지(){
-        //given db에 저장되어있는 친구 정보
+        //given 기존 db에 저장되어있는 친구 정보
         Movie originMovie = Movie.builder().
             movieCountry("en").movieGenre("12").movieRunningTime(123).movieDirector("123")
             .movieDetail("fwefsv").movieDate("1234").movieActor("12345")
             .title("helloworld2").movieImg("1234").movieBgImg("12345").real_movieId(444332L).build();
 
         //when
-
         Optional<Movie> helloworld = movieRepository.findByTitle("helloworld2");
         Movie findMovie = helloworld.get();
 
         //then
-
-        Assertions.assertThat(originMovie).isNotEqualTo(findMovie);
+        Assertions.assertThat(originMovie).isEqualTo(findMovie);
         // -> DB에 이후에 먼저 저장되어있는 친구를 찾아서 기존의 친구의 형태와 비교하면 다르게 됨.
         //1차 캐시는 트랜잭션이 끝나기 직전에는 계속 존재하고 있음.
         //flush는 영속성 컨텍스트를 비우는게 아니라 변경내용을 데이터베이스에 동기화 하는 작업임.
-
     }
 
 
@@ -93,12 +94,12 @@ public class SaveTest {
             .title("helloworld2").movieImg("1234").movieBgImg("12345").real_movieId(444332L).build();
 
         //when
-
         Optional<Movie> helloworld = movieRepository.findByTitle("helloworld2");
         Movie findMovie = helloworld.get();
         Long findMovieId = findMovie.getId();
         findMovie.setMovieCountry("china");
-        movieRepository.save(findMovie);
+        movieRepository.save(findMovie); // 이렇게 하면 변경이 진행됨.
+
         //then
         Optional<Movie> checkMovie = movieRepository.findById(findMovieId);
         Movie changeValueMovie = checkMovie.get();
