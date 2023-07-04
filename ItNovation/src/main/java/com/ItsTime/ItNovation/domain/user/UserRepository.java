99bd1d885b4d3c9ea346ac38ddc186f3e1f9b7d9
khyojun <nandong5923@naver.com>
2023-07-04 15:51:01 +0900

    [feat] 유저 이름 검색에 따른 user 목록 긁어오는 jpql 문 작성 및 메서드 추가

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/user/UserRepository.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/user/UserRepository.java
@@ -1,11 +1,18 @@
 package com.ItsTime.ItNovation.domain.user;
 
+import java.util.List;
+import javax.swing.text.html.Option;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findById(Long id);
     Optional<User> findByRefreshToken(String refreshToken);
+
+    @Query("SELECT u FROM User u WHERE u.nickname = :nickname OR u.nickname LIKE %:nickname%")
+    List<User> findByNickname(@Param("nickname")String nickname);
 }
