eac6024da93b4fefc686bab2eb2da10d3c8667df
khyojun <nandong5923@naver.com>
2023-07-06 02:44:01 +0900

    [refactor] 메서드 분리를 통한 코드 리팩토링

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/best/TodayBestReviewService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/best/TodayBestReviewService.java
@@ -3,15 +3,10 @@ package com.ItsTime.ItNovation.service.best;
 import com.ItsTime.ItNovation.domain.bestReview.dto.TodayBestReviewDto;
 import com.ItsTime.ItNovation.domain.bestReview.dto.TodayBestReviewResponseDto;
 import com.ItsTime.ItNovation.domain.movie.Movie;
-import com.ItsTime.ItNovation.domain.movie.MovieRepository;
 import com.ItsTime.ItNovation.domain.movie.dto.TodayBestReviewMovieDto;
 import com.ItsTime.ItNovation.domain.review.Review;
-import com.ItsTime.ItNovation.domain.review.ReviewRepository;
 import com.ItsTime.ItNovation.domain.reviewLike.ReviewLikeRepository;
 import com.ItsTime.ItNovation.domain.user.User;
-import com.ItsTime.ItNovation.domain.user.UserRepository;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -26,14 +21,11 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class TodayBestReviewService {
 
-    private final MovieRepository movieRepository;
-    private final ReviewRepository reviewRepository;
     private final ReviewLikeRepository reviewLikeRepository;
-    private final UserRepository userRepository;
 
 
     public ResponseEntity getBestReviewAndUser() {
-        Pageable pageable = PageRequest.of(0,3);
+        Pageable pageable = PageRequest.of(0, 3);
         List<User> top3UsersWithTodayDate = reviewLikeRepository.findTop3UsersWithTodayDate(
             pageable);
         try {
@@ -43,8 +35,6 @@ public class TodayBestReviewService {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(400).body(e.getMessage());
         }
-
-
     }
 
     private List<TodayBestReviewResponseDto> madeResponse(List<User> top3UsersWithTodayDate) {
@@ -55,46 +45,52 @@ public class TodayBestReviewService {
             throw new IllegalArgumentException("오늘 눌러진 좋아요가 없습니다.");
         }
         for (User user : top3UsersWithTodayDate) {
-            TodayBestReviewResponseDto bestReviewResponseDto = TodayBestReviewResponseDto.builder()
-                .userId(user.getId())
-                .profileImg(user.getProfileImg())
-                .nickName(user.getNickname())
-                .introduction(user.getIntroduction())
-                .reviews(findTodayTop2Review(user))
-                .build();
-
+            TodayBestReviewResponseDto bestReviewResponseDto = convertToDto(user);
             todayBestReviewResponseDtos.add(bestReviewResponseDto);
         }
-
         return todayBestReviewResponseDtos;
     }
 
+    private TodayBestReviewResponseDto convertToDto(User user) {
+        TodayBestReviewResponseDto bestReviewResponseDto = TodayBestReviewResponseDto.builder()
+            .userId(user.getId())
+            .profileImg(user.getProfileImg())
+            .nickName(user.getNickname())
+            .introduction(user.getIntroduction())
+            .reviews(findTodayTop2Review(user))
+            .build();
+        return bestReviewResponseDto;
+    }
+
     private List<TodayBestReviewDto> findTodayTop2Review(User user) {
-        Pageable pageable = PageRequest.of(0,2);
+        Pageable pageable = PageRequest.of(0, 2);
         List<TodayBestReviewDto> reviewDtos = new ArrayList<>();
         List<Review> top2ReviewsByUserId = reviewLikeRepository.findTop2ReviewsByUserId(
             user.getId(), pageable);
 
-        for (int i=0; i<2; i++) {
-            Review review = top2ReviewsByUserId.get(i);
-            TodayBestReviewDto buildReviewDto = TodayBestReviewDto.builder()
-                .reviewId(review.getReviewId())
-                .reviewTitle(review.getReviewTitle())
-                .movie(madeDtoMovie(review.getMovie()))
-                .build();
-
-            reviewDtos.add(buildReviewDto);
-        }
-
+        convertToTopBestReviewDtos(reviewDtos, top2ReviewsByUserId);
 
         return reviewDtos;
     }
 
-    private TodayBestReviewMovieDto madeDtoMovie(Movie movie) {
-            return TodayBestReviewMovieDto.builder()
-                .movieId(movie.getId())
-                .movieImg(movie.getMovieImg())
+    private void convertToTopBestReviewDtos(List<TodayBestReviewDto> reviewDtos,
+        List<Review> top2ReviewsByUserId) {
+        for (int i = 0; i < 2; i++) {
+            Review review = top2ReviewsByUserId.get(i);
+            TodayBestReviewDto buildReviewDto = TodayBestReviewDto.builder()
+                .reviewId(review.getReviewId())
+                .reviewTitle(review.getReviewTitle())
+                .movie(convertToBestReviewMovieDto(review.getMovie()))
                 .build();
+            reviewDtos.add(buildReviewDto);
+        }
+    }
+
+    private TodayBestReviewMovieDto convertToBestReviewMovieDto(Movie movie) {
+        return TodayBestReviewMovieDto.builder()
+            .movieId(movie.getId())
+            .movieImg(movie.getMovieImg())
+            .build();
     }
 
 }
