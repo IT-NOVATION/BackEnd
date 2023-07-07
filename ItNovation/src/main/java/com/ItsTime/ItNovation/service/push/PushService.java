77d13fe9652a01678bc351a2f5c59a4d01dbf947
khyojun <nandong5923@naver.com>
2023-07-07 14:24:02 +0900

    [feat] 팔로우 버튼을 눌렀을때 작동하는 로직 추가

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/push/PushService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/push/PushService.java
@@ -1,6 +1,9 @@
 package com.ItsTime.ItNovation.service.push;
 
 
+import com.ItsTime.ItNovation.domain.follow.FollowRepository;
+import com.ItsTime.ItNovation.domain.follow.Follower;
+import com.ItsTime.ItNovation.domain.follow.dto.FollowStateResponseDto;
 import com.ItsTime.ItNovation.domain.review.Review;
 import com.ItsTime.ItNovation.domain.review.ReviewRepository;
 import com.ItsTime.ItNovation.domain.review.dto.PushReviewLikeResponseDto;
@@ -25,6 +28,7 @@ public class PushService {
     private final UserRepository userRepository;
     private final ReviewRepository reviewRepository;
     private final ReviewLikeRepository reviewLikeRepository;
+    private final FollowRepository followRepository;
 
     @Transactional
     public ResponseEntity pushReviewLike(Long reviewId, Long pushUserId) {
@@ -76,5 +80,46 @@ public class PushService {
             .reviewLike(true)
             .build();
         return build;
+    }
+
+    public ResponseEntity pushFollow(Long pushUserId, Long targetId) {
+        try {
+            User pushUser = userRepository.findById(pushUserId)
+                .orElseThrow(() -> new IllegalArgumentException("해당 유저가 없어요!"));
+            User targetUser = userRepository.findById(targetId).orElseThrow(() -> new IllegalArgumentException("해당 팔로우 유저가 없어요!"));
+            //Optional<Follower> findByPushUser = followRepository.findByPushUser(pushUser);
+            Optional<Follower> findByPushUserAndFollowUser = followRepository.findByPushUserAndFollowUser(
+                pushUser.getId(), targetUser.getId());
+            if(pushUser.getId().equals(targetUser.getId())){
+                throw new IllegalArgumentException("자기가 자기 자신을 팔로우 할 순 없습니다!");
+            }
+            if(findByPushUserAndFollowUser.isEmpty()){
+                Follower build = Follower.builder()
+                    .pushUser(pushUser)
+                    .follower(targetUser)
+                    .build();
+                followRepository.save(build);
+                FollowStateResponseDto followStateResponseDto = FollowStateResponseDto.builder()
+                    .isFollow(true).build();
+                return ResponseEntity.status(200).body(followStateResponseDto);
+            }
+            else{
+                Follower find = findByPushUserAndFollowUser.get();
+
+                // 단순히 지울 작업이 아니라 follow id pushUser id 두개 다 비교해서 일치하면 지우고 아니면
+                // 만들고 해야함.
+
+                followRepository.delete(find);
+                FollowStateResponseDto followStateResponseDto = FollowStateResponseDto.builder()
+                    .isFollow(false).build();
+                return ResponseEntity.status(200).body(followStateResponseDto);
+            }
+        }catch (IllegalArgumentException e){
+            return ResponseEntity.status(400).body(e.getMessage());
+        }
+
+
+
+
     }
 }
