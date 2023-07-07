f40fcd275f1f78a2dba66b9e6a315c47da6f60ae
khyojun <nandong5923@naver.com>
2023-07-07 16:39:09 +0900

    [feat] topUser에 대하여 보여주기 위한 reviewLikeRepo에서 필요한 내용 jpql 작성

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/reviewLike/ReviewLikeRepository.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/reviewLike/ReviewLikeRepository.java
@@ -2,10 +2,8 @@ package com.ItsTime.ItNovation.domain.reviewLike;
 
 import com.ItsTime.ItNovation.domain.review.Review;
 import com.ItsTime.ItNovation.domain.user.User;
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -22,17 +20,28 @@ public interface ReviewLikeRepository extends JpaRepository<ReviewLike, Long> {
         "AND DATE(rl.createdDate) = CURRENT_DATE " +
         "GROUP BY rl.user " +
         "ORDER BY COUNT(rl.user) DESC")
-    List<User> findTop3UsersWithTodayDate(Pageable pageable);
+    List<User> findTopUsersWithTodayDate(Pageable pageable);
 
 
     @Query("SELECT r FROM Review r " +
         "WHERE r.user.id = :userId " +
         "ORDER BY (SELECT COUNT(rl) FROM ReviewLike rl WHERE rl.review = r AND rl.reviewLike = true AND DATE(rl.createdDate) = CURRENT_DATE) DESC")
-    List<Review> findTop2ReviewsByUserId(@Param("userId") Long userId, Pageable pageable);
+    List<Review> findTopReviewsByUserId(@Param("userId") Long userId, Pageable pageable);
+
+
+    @Query("SELECT r FROM Review r " +
+        "WHERE r.user.id = :userId " +
+        "ORDER BY (SELECT COUNT(rl) FROM ReviewLike rl WHERE rl.review = r AND rl.reviewLike = true AND DATE(rl.createdDate) = CURRENT_DATE) DESC")
+    List<Review> bestReviewsByUserId(@Param("userId") Long userId);
 
 
 
     @Query("select rl from ReviewLike rl where rl.user.id = :userId AND rl.review.reviewId= :reviewId")
     Optional<ReviewLike> findReviewLikeByReviewIdAndUserId(@Param("userId") Long userId, @Param("reviewId") Long reviewId);
+
+
+
+    @Query("select count(*) from ReviewLike rl where rl.review.reviewId=:reviewId and rl.reviewLike=true")
+    int countReviewLikeByReviewId(@Param("reviewId") Long reviewId);
 
 }
