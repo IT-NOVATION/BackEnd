0286a799ae9881366be067e53b4ee476d5a8e006
khyojun <nandong5923@naver.com>
2023-07-07 12:43:25 +0900

    [chore] 패키지 분리 및 공통된 기능으로 인하여 포괄적으로 이름 수정

--- ItNovation/src/main/java/com/ItsTime/ItNovation/controller/push/PushController.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/push/PushController.java
@@ -1,4 +1,4 @@
-package com.ItsTime.ItNovation.controller.review;
+package com.ItsTime.ItNovation.controller.push;
 
 
 import com.ItsTime.ItNovation.service.push.PushService;
@@ -6,18 +6,20 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/push")
 @Slf4j
-public class PushReviewLikeController {
+public class PushController {
 
     private final PushService pushService;
 
 
-    @GetMapping("/push/reviewlike")
+    @GetMapping("/reviewlike")
     public ResponseEntity pushReviewLike(@RequestParam(name="reviewId") Long reviewId, @RequestParam(name="userId") Long userId){
         log.info(reviewId.toString());
         log.info(userId.toString());
@@ -26,13 +28,17 @@ public class PushReviewLikeController {
     }
 
 
-
-
-
-
-
-
-
-
+    /**
+     * 1. follow 눌렀을때 상대방과 follow가 안돼있는 case
+     * 2. follow 테이블이 존재하는데 해제하고 싶은 case
+     *
+     * @param targetId
+     * @param pushUserId
+     * @return response
+     */
+    @GetMapping("/follow")
+    public ResponseEntity pushFollow(@RequestParam(name="pushUserId") Long pushUserId, @RequestParam(name="targetUserId") Long targetId){
+        return pushService.pushFollow(pushUserId, targetId);
+    }
 
 }
