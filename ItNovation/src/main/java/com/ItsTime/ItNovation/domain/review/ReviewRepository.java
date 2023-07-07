ac7ffaac31a54798042a357ee4a71701b088674e
khyojun <nandong5923@naver.com>
2023-07-07 16:39:29 +0900

    [feat] topUser에 대하여 보여주기 위한 reviewRepo에서 필요한 내용 jpql 작성

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/review/ReviewRepository.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/review/ReviewRepository.java
@@ -1,11 +1,23 @@
 package com.ItsTime.ItNovation.domain.review;
 
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     Optional<Review> findById(Long ReviewId);
+
+
+
+    @Query("Select r from Review r where r.user.id=:userId order by r.createdDate desc")
+    List<Review> findNewestReviewByUserId(@Param("userId") Long userId, Pageable pageable);
+
+
 
 }
