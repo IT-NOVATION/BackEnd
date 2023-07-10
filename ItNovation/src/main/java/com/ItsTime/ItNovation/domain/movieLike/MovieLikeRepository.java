251d8791dd42b3de77ef5e493010914c2e9f6d41
khyojun <nandong5923@naver.com>
2023-07-10 23:08:18 +0900

    [feat] 영화 좋아요 갯수 counting 기능 관련 jpql 추가

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/movieLike/MovieLikeRepository.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/movieLike/MovieLikeRepository.java
@@ -1,8 +1,15 @@
 package com.ItsTime.ItNovation.domain.movieLike;
 
+import com.ItsTime.ItNovation.domain.movie.Movie;
 import com.ItsTime.ItNovation.domain.star.Star;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface MovieLikeRepository extends JpaRepository<Star, Long> {
+public interface MovieLikeRepository extends JpaRepository<MovieLike, Long> {
 
+
+
+    @Query("select count(ml) from MovieLike ml where ml.movie = :movie")
+    int countMovieLike(@Param("movie") Movie movie);
 }
