8ed929a204d9b17303b3c80e6a496895e60d498c
khyojun <nandong5923@naver.com>
2023-07-10 19:56:40 +0900

    [feat] 개별영화페이지 컨트롤러 구현

---
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/singleMoviePage/SingleMoviePageController.java
@@ -0,0 +1,28 @@
+package com.ItsTime.ItNovation.controller.singleMoviePage;
+
+
+import com.ItsTime.ItNovation.service.single.SingleMoviePageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/single")
+@Slf4j
+public class SingleMoviePageController {
+
+    private final SingleMoviePageService singleMoviePageService;
+
+    @GetMapping("/moviePage/{movieId}")
+    public ResponseEntity moviePage(@PathVariable Long movieId){
+        return singleMoviePageService.getReviewInformationAboutMovie(movieId);
+    }
+
+
+}
