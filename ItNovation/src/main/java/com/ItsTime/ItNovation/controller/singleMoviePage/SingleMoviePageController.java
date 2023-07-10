7b247a07d3c6f1e26133534811bc008935341ccf
khyojun <nandong5923@naver.com>
2023-07-10 22:48:58 +0900

    [feat] singleMoviePage 컨트롤러 생성

--- ItNovation/src/main/java/com/ItsTime/ItNovation/controller/singleMoviePage/SingleMoviePageController.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/singleMoviePage/SingleMoviePageController.java
@@ -1,7 +1,7 @@
 package com.ItsTime.ItNovation.controller.singleMoviePage;
 
 
-import com.ItsTime.ItNovation.service.single.SingleMoviePageService;
+import com.ItsTime.ItNovation.service.singleMoviePage.SingleMoviePageService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
