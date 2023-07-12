d7a2afbd29a6a2f5181ba4214af1073ba3d6b206
djc06048 <gsafe1213@gmail.com>
2023-07-12 21:33:15 +0900

    [feat] 팔로워 가져오는 메서드 추가
    followstate 관련 엔티티 수정 반영

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/follow/FollowRepository.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/follow/FollowRepository.java
@@ -1,6 +1,8 @@
 package com.ItsTime.ItNovation.domain.follow;
 
 import com.ItsTime.ItNovation.domain.user.User;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,20 +13,20 @@ public interface FollowRepository extends JpaRepository<FollowState, Long> {
 
     Optional<FollowState> findByPushUser(User user);
 
-    @Query("Select f from FollowState  f where f.pushUser.id=:pushUser and f.follower.id=:targetUser")
+    @Query("Select f from FollowState  f where f.pushUser.id=:pushUser and f.targetUser.id=:targetUser")
     Optional<FollowState> findByPushUserAndFollowUser(@Param("pushUser") Long pushUserId, @Param("targetUser") Long targetUserId);
 
 
-    @Query("delete FollowState f where f.pushUser.id=:pushUser and f.follower.id=:targetUser")
+    @Query("delete FollowState f where f.pushUser.id=:pushUser and f.targetUser.id=:targetUser")
     void deleteByPushUserAndFollowUser(@Param("pushUser") Long pushUserId, @Param("targetUser") Long targetUserId);
-
 
 
     Optional<FollowState> findById(Long id);
 
-    @Query("select count(*) from FollowState f where f.follower.id=:userId")
+    @Query("select count(*) from FollowState f where f.targetUser.id=:userId")
     Long countByFollowedUserId(@Param("userId") Long userId);
 
-
+    @Query("SELECT f.pushUser FROM FollowState f WHERE f.targetUser.id = :userId")
+    List<User> findFollowersByUserId(@Param("userId") Long userId);
 
 }
