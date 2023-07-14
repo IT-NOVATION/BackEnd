f7f69b3763307ee28d13297614e7adf56b5c797f
[J-neat] <[junghyo0258@catholic.ac.kr]>
2023-07-14 21:31:50 +0900

    [feat] TopFollowerController 작성

---
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/topfollower/TopFollowerController.java
@@ -0,0 +1,24 @@
+package com.ItsTime.ItNovation.controller.topfollower;
+
+import com.ItsTime.ItNovation.domain.topFollower.TopFollowerResponseDto;
+import com.ItsTime.ItNovation.service.topFollower.TopFollowerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class TopFollowerController {
+    private final TopFollowerService topFollowerService;
+
+    @GetMapping("/test/today/popularUser")
+    public ResponseEntity<List<TopFollowerResponseDto>> getTopFollowers() {
+        List<TopFollowerResponseDto> topFollowers = topFollowerService.getTopFollowers();
+        return ResponseEntity.ok(topFollowers);
+    }
+}
---
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/topFollower/TopFollowerService.java
@@ -0,0 +1,51 @@
+package com.ItsTime.ItNovation.service.topFollower;
+
+import com.ItsTime.ItNovation.domain.follow.FollowRepository;
+import com.ItsTime.ItNovation.domain.movie.dto.FollowMovieResponseDto;
+import com.ItsTime.ItNovation.domain.review.ReviewRepository;
+import com.ItsTime.ItNovation.domain.review.dto.TopFollowerReviewResponseDto;
+import com.ItsTime.ItNovation.domain.topFollower.TopFollowerResponseDto;
+import com.ItsTime.ItNovation.domain.user.User;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class TopFollowerService {
+    private final ReviewRepository reviewRepository;
+    private final FollowRepository followRepository;
+
+    public List<TopFollowerResponseDto> getTopFollowers() {
+        List<User> topPushUsers = followRepository.findTop3PushUsersByTargetUserCount();
+
+        return topPushUsers.stream().map(topPushUser -> {
+            List<TopFollowerReviewResponseDto> reviews = reviewRepository.findNewestReviewByUserId(topPushUser.getId(), PageRequest.of(0, 3))
+                    .stream()
+                    .map(review -> TopFollowerReviewResponseDto.builder()
+                            .reviewId(review.getReviewId())
+                            .reviewTitle(review.getReviewTitle())
+                            .movie(FollowMovieResponseDto.builder()
+                                    .movieId(review.getMovie().getId())
+                                    .movieImg(review.getMovie().getMovieImg())
+                                    .build())
+                            .build())
+                    .collect(Collectors.toList());
+
+            return TopFollowerResponseDto.builder()
+                    .userId(topPushUser.getId())
+                    .profileImg(topPushUser.getProfileImg())
+                    .nickName(topPushUser.getNickname())
+                    .introduction(topPushUser.getIntroduction())
+                    .reviews(reviews)
+                    .build();
+        }).collect(Collectors.toList());
+    }
+}
+
