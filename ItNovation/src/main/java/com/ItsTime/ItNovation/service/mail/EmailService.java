197f6d2e10b07791d4efa6ee5ae7025ead836a92
djc06048 <gsafe1213@gmail.com>
2023-07-15 20:16:00 +0900

    [refactor] 코드 위치 수정

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/mail/EmailService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/mail/EmailService.java
@@ -7,14 +7,17 @@ import java.io.UnsupportedEncodingException;
 import java.util.Map;
 import java.util.Random;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
@@ -27,7 +30,31 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String from;
 
+
+    public String sendMail(String toEmail) throws MessagingException, UnsupportedEncodingException{
+        log.info("senemail service");
+        MimeMessage emailForm = createEmailForm(toEmail);
+        javaMailSender.send(emailForm);
+        log.info("javamailsender");
+        return authNum;
+    }
+    public MimeMessage createEmailForm(String email) throws MessagingException, UnsupportedEncodingException{
+        createCode();
+        String toEmail = email;
+        String title = "MOVIELOG 비밀번호 교체 관련 인증 번호";
+
+        MimeMessage message = javaMailSender.createMimeMessage(); //MimeMessage 객체를 직접 생성하여 메일을 발송하는 방법
+
+        message.addRecipients(RecipientType.TO, toEmail);
+        message.setSubject(title);
+        message.setFrom(from);
+        message.setText(setContext(authNum), "utf-8", "html");
+        log.info(email);
+        return message;
+    }
+
     public void createCode() { // 인증번호 무작위 6자리 -> 문자로 들어가게 됨.
+
         Random random = new Random();
 
         StringBuffer key = new StringBuffer();
@@ -37,31 +64,12 @@ public class EmailService {
             key.append(Integer.toString(index));
             authNum = key.toString();
         }
-        System.out.println(authNum);
-    }
-
-    public MimeMessage createEmailForm(String email) throws MessagingException, UnsupportedEncodingException{
-        createCode();
-        String toEmail = email;
-        String title = "MOVIELOG 비밀번호 교체 관련 인증 번호";
-
-        MimeMessage message = javaMailSender.createMimeMessage(); //MimeMessage 객체를 직접 생성하여 메일을 발송하는 방법
-        message.addRecipients(RecipientType.TO, toEmail);
-        message.setSubject(title);
-        message.setFrom(from);
-        message.setText(setContext(authNum), "utf-8", "html");
-        System.out.println(email);
-        return message;
+        log.info(authNum);
     }
 
 
-    public String sendMail(String toEmail) throws MessagingException, UnsupportedEncodingException{
 
-        MimeMessage emailForm = createEmailForm(toEmail);
 
-        javaMailSender.send(emailForm);
-        return authNum;
-    }
 
 
 
