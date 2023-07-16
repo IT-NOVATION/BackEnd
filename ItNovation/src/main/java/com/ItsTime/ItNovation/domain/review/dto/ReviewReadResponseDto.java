7d83162aef15f722bd709256e819a32e7b2b6ada
khyojun <nandong5923@naver.com>
2023-07-16 19:36:39 +0900

    [feat] loginUser에 대한 정보를 추가하는 필드 추가

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/review/dto/ReviewReadResponseDto.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/review/dto/ReviewReadResponseDto.java
@@ -2,6 +2,7 @@ package com.ItsTime.ItNovation.domain.review.dto;
 
 
 import com.ItsTime.ItNovation.domain.movie.dto.ReviewMovieInfoDto;
+import com.ItsTime.ItNovation.domain.user.dto.ReviewLoginUserInfoDto;
 import com.ItsTime.ItNovation.domain.user.dto.ReviewUserInfoDto;
 import lombok.AccessLevel;
 import lombok.Builder;
@@ -15,13 +16,15 @@ public class ReviewReadResponseDto {
     private ReviewInfoDto review;
     private ReviewMovieInfoDto movie;
     private ReviewUserInfoDto user;
+    private ReviewLoginUserInfoDto loginUser;
 
 
     @Builder
     public ReviewReadResponseDto(ReviewInfoDto review, ReviewMovieInfoDto movie,
-        ReviewUserInfoDto user) {
+        ReviewUserInfoDto user, ReviewLoginUserInfoDto loginUser) {
         this.review = review;
         this.movie = movie;
         this.user = user;
+        this.loginUser = loginUser;
     }
 }
