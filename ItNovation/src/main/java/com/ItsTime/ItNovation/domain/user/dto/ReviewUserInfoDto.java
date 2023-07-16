bf67d9f30d47fa3c630dbb660c9728e1944515c1
khyojun <nandong5923@naver.com>
2023-07-16 19:47:08 +0900

    [feat] following, follower 변수 이름 수정 및 추가

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/user/dto/ReviewUserInfoDto.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/user/dto/ReviewUserInfoDto.java
@@ -14,6 +14,9 @@ public class ReviewUserInfoDto {
     private Grade grade;
     private String introduction;
     private String profileImg;
+    private Long folllowerNum;
+    private Long followingNum;
+    private Boolean hasReviewLike;
 
 //    followerNum:Long,
 //        "hasReviewLike":Boolean,  추후 해당 기능 추가 후 추가 예정
@@ -21,12 +24,15 @@ public class ReviewUserInfoDto {
 
     @Builder
     public ReviewUserInfoDto(Long userId, String bgImg, String nickname, Grade grade,
-        String introduction, String profileImg) {
+        String introduction, String profileImg, Long folllowerNum, Long followingNum, Boolean hasReviewLike) {
         this.userId = userId;
         this.bgImg = bgImg;
         this.nickname = nickname;
         this.grade = grade;
         this.introduction = introduction;
         this.profileImg = profileImg;
+        this.folllowerNum = folllowerNum;
+        this.followingNum = followingNum;
+        this.hasReviewLike=hasReviewLike;
     }
 }
