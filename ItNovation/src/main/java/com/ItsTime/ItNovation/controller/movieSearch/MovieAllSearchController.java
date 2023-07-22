7173107440ce209191598dfc317c4fa1bd379437
djc06048 <gsafe1213@gmail.com>
2023-07-22 16:15:07 +0900

    [feat] 리뷰수 많은 기준 무비서치 컨트롤러 추가

---
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/movieSearch/MovieAllSearchController.java
@@ -0,0 +1,26 @@
+package com.ItsTime.ItNovation.controller.movieSearch;
+
+import com.ItsTime.ItNovation.domain.movielog.dto.MovieLogResponseDto;
+import com.ItsTime.ItNovation.service.movieSearch.MovieAllSearchService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/movie-search")
+public class MovieAllSearchController {
+    private final MovieAllSearchService movieAllSearchService;
+
+    @GetMapping("/review-order/{page}")
+    public ResponseEntity getMoiveSearchResponseByReviewOrder(@PathVariable int page) {
+
+        return movieAllSearchService.getMoiveSearchResponseByReviewOrder(page);
+    }
+}
