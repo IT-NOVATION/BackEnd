a9842aca46a4d5dab000547280e8b9d959c270df
[J-neat] <[junghyo0258@catholic.ac.kr]>
2023-07-22 17:13:52 +0900

    [feat] 팔로우 여부와 내 프로필인지 여부 추가

--- ItNovation/src/main/java/com/ItsTime/ItNovation/domain/topFollower/TopFollowerResponseDto.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/domain/topFollower/TopFollowerResponseDto.java
@@ -10,17 +10,22 @@ import java.util.List;
 @Getter
 public class TopFollowerResponseDto {
     private Long userId;
+    private Boolean isMyProfile;
+    private Boolean loginUserPushedFollow;
     private String profileImg;
     private String nickName;
     private String introduction;
     private List<TopFollowerReviewResponseDto> reviews;
 
     @Builder
-    public TopFollowerResponseDto(Long userId, String profileImg, String nickName, String introduction, List<TopFollowerReviewResponseDto> reviews) {
+    public TopFollowerResponseDto(Long userId, Boolean isMyProfile, Boolean loginUserPushedFollow, String profileImg, String nickName, String introduction, List<TopFollowerReviewResponseDto> reviews) {
         this.userId = userId;
+        this.isMyProfile = isMyProfile;
+        this.loginUserPushedFollow = loginUserPushedFollow;
         this.profileImg = profileImg;
         this.nickName = nickName;
         this.introduction = introduction;
         this.reviews = reviews;
     }
+
 }
