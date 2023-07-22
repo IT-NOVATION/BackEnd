3fcee203c8cd3569dad26e19336bcff721d6ee8b
[J-neat] <[junghyo0258@catholic.ac.kr]>
2023-07-22 17:11:52 +0900

    [fix] HttpServletRequest 받는 형식으로 컨트롤러 수정러

--- ItNovation/src/main/java/com/ItsTime/ItNovation/controller/topfollower/TopFollowerController.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/topfollower/TopFollowerController.java
@@ -1,7 +1,9 @@
 package com.ItsTime.ItNovation.controller.topfollower;
 
 import com.ItsTime.ItNovation.domain.topFollower.TopFollowerResponseDto;
+import com.ItsTime.ItNovation.jwt.service.JwtService;
 import com.ItsTime.ItNovation.service.topFollower.TopFollowerService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -9,16 +11,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
 @RestController
 public class TopFollowerController {
     private final TopFollowerService topFollowerService;
+    private final JwtService jwtService;
 
-    @GetMapping("/today/popularUser")
-    public ResponseEntity<List<TopFollowerResponseDto>> getTopFollowers() {
-        List<TopFollowerResponseDto> topFollowers = topFollowerService.getTopFollowers();
-        return ResponseEntity.ok(topFollowers);
+    @GetMapping("/test/today/popularUser")
+    public ResponseEntity<List<TopFollowerResponseDto>> getTopFollowers(HttpServletRequest request) {
+        Optional<String> s = jwtService.extractAccessToken(request);
+        if(s.isPresent()){
+            Optional<String> email = jwtService.extractEmail(s.get());
+            if(email.isPresent()) {
+                List<TopFollowerResponseDto> topFollowers = topFollowerService.getTopFollowers(email.get());
+                return ResponseEntity.ok(topFollowers);
+            }
+        }
+        List<TopFollowerResponseDto> topFollowers = topFollowerService.getTopFollowers(null);
+        return ResponseEntity.status(200).body(topFollowers);
     }
 }
