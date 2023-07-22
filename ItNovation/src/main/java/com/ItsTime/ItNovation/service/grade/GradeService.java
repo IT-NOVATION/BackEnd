28d97db03c9b9e8c54a67b1a0be0e6892e86c571
khyojun <nandong5923@naver.com>
2023-07-22 11:44:00 +0900

    [fix] gradeService 충돌 해결

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/grade/GradeService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/grade/GradeService.java
@@ -1,11 +1,25 @@
 package com.ItsTime.ItNovation.service.grade;
 
+
+import com.ItsTime.ItNovation.domain.comment.CommentRepository;
+import com.ItsTime.ItNovation.domain.review.ReviewRepository;
+import com.ItsTime.ItNovation.domain.user.User;
+import com.ItsTime.ItNovation.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class GradeService {
 
 
+    private final CommentRepository commentRepository;
+    private final ReviewRepository reviewRepository;
+
+    public void validateGrade(User findUser) {
+
+        commentRepository.findAllByUserId(findUser.getId());
 
 
+    }
 }
