e0fb3e7008d70132e651c59d733fb3d733e72a48
djc06048 <gsafe1213@gmail.com>
2023-07-22 23:34:19 +0900

    [fix] 무비토크 인기유저 서비스 수정

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/topFollower/TopFollowerService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/topFollower/TopFollowerService.java
@@ -1,107 +1,102 @@
 package com.ItsTime.ItNovation.service.topFollower;
-
+import com.ItsTime.ItNovation.common.GeneralErrorCode;
 import com.ItsTime.ItNovation.domain.follow.FollowRepository;
 import com.ItsTime.ItNovation.domain.follow.FollowState;
 import com.ItsTime.ItNovation.domain.movie.dto.FollowMovieResponseDto;
-import com.ItsTime.ItNovation.domain.review.Review;
 import com.ItsTime.ItNovation.domain.review.ReviewRepository;
 import com.ItsTime.ItNovation.domain.review.dto.TopFollowerReviewResponseDto;
 import com.ItsTime.ItNovation.domain.topFollower.TopFollowerResponseDto;
 import com.ItsTime.ItNovation.domain.user.User;
 import com.ItsTime.ItNovation.domain.user.UserRepository;
+import com.ItsTime.ItNovation.jwt.service.JwtService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.HTTP;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
-
-
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class TopFollowerService {
     private final ReviewRepository reviewRepository;
     private final FollowRepository followRepository;
+    private final JwtService jwtService;
     private final UserRepository userRepository;
+    private String nowUserEmail = null;
 
     @Transactional
-    public List<TopFollowerResponseDto> getTopFollowers(String email) {
-        List<User> topPushUsers = followRepository.findTop3TargetUsersByPushUserCount();
-        List<TopFollowerResponseDto> topFollowers = new ArrayList<>();
+    public ResponseEntity getTopFollowers(Optional<String> accessToken) {
+        if (accessToken.isPresent()) {
+            nowUserEmail = jwtService.extractEmail(accessToken.get()).get();
+        }
+        log.info(nowUserEmail);
+        Pageable pageable = PageRequest.of(0, 2);
+        List<User> topPushUsers = followRepository.findTop3PushUsersByTargetUserCount(pageable);
 
-        for (User topPushUser : topPushUsers) {
-            List<TopFollowerReviewResponseDto> reviews = new ArrayList<>();
-            List<Review> newestReviews = reviewRepository.findNewestReviewByUserId(topPushUser.getId(), PageRequest.of(0, 2));
-
-            for (Review review : newestReviews) {
-                FollowMovieResponseDto movieDto = getFollowMovieResponseDto(review);
-                TopFollowerReviewResponseDto reviewDto = getTopFollowerReviewResponseDto(review, movieDto);
-                reviews.add(reviewDto);
-            }
-            TopFollowerResponseDto followerDto = getTopFollowerResponseDto(email, topPushUser, reviews);
-            topFollowers.add(followerDto);
+        if (topPushUsers.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(GeneralErrorCode.NO_REVIEW.getMessage());
+        } else {
+            List<TopFollowerResponseDto> topFollowerResponseDtoList=topPushUsers.stream().map(topPushUser -> {
+                log.info(String.valueOf(topPushUser.getId()));
+                List<TopFollowerReviewResponseDto> reviews = reviewRepository.findNewestReviewByUserId(topPushUser.getId(), PageRequest.of(0, 3))
+                        .stream()
+                        .map(review -> TopFollowerReviewResponseDto.builder()
+                                .reviewId(review.getReviewId())
+                                .reviewTitle(review.getReviewTitle())
+                                .movie(FollowMovieResponseDto.builder()
+                                        .movieId(review.getMovie().getId())
+                                        .movieImg(review.getMovie().getMovieImg())
+                                        .build())
+                                .build())
+                        .collect(Collectors.toList());
+                return TopFollowerResponseDto.builder()
+                        .userId(topPushUser.getId())
+                        .profileImg(topPushUser.getProfileImg())
+                        .isMyProfile(profileState(nowUserEmail, topPushUser.getId()))
+                        .isNowUserFollowThisUser(followState(nowUserEmail, topPushUser.getId()))
+                        .nickName(topPushUser.getNickname())
+                        .introduction(topPushUser.getIntroduction())
+                        .reviews(reviews)
+                        .build();
+            }).collect(Collectors.toList());
+            return ResponseEntity.status(HttpStatus.OK).body(topFollowerResponseDtoList);
         }
 
-        return topFollowers;
     }
 
-    private TopFollowerResponseDto getTopFollowerResponseDto(String email, User topPushUser, List<TopFollowerReviewResponseDto> reviews) {
-        TopFollowerResponseDto followerDto = TopFollowerResponseDto.builder()
-                .userId(topPushUser.getId())
-                .isMyProfile(profileState(email, topPushUser.getId()))
-                .loginUserPushedFollow(followState(email, topPushUser.getId()))
-                .profileImg(topPushUser.getProfileImg())
-                .nickName(topPushUser.getNickname())
-                .introduction(topPushUser.getIntroduction())
-                .reviews(reviews)
-                .build();
-        return followerDto;
-    }
+    private Boolean profileState(String nowUserEmail, Long userId) {
+        Optional<User> nowUser = userRepository.findByEmail(nowUserEmail);
+        log.info(String.valueOf(nowUser.get().getId()));
 
-    private static TopFollowerReviewResponseDto getTopFollowerReviewResponseDto(Review review, FollowMovieResponseDto movieDto) {
-        TopFollowerReviewResponseDto reviewDto = TopFollowerReviewResponseDto.builder()
-                .reviewId(review.getReviewId())
-                .reviewTitle(review.getReviewTitle())
-                .movie(movieDto)
-                .build();
-        return reviewDto;
-    }
-
-    private static FollowMovieResponseDto getFollowMovieResponseDto(Review review) {
-        FollowMovieResponseDto movieDto = FollowMovieResponseDto.builder()
-                .movieId(review.getMovie().getId())
-                .movieImg(review.getMovie().getMovieImg())
-                .build();
-        return movieDto;
-    }
-
-    public Boolean profileState(String email, Long userId) {
-        Optional<User> user = userRepository.findByEmail(email);
         Optional<User> checkUser = userRepository.findById(userId);
-        if(user.isPresent() && user == checkUser){
-            return false;//로그인 했으며 자신의 프로필이므로 false 반환
-        }
-        return true;
+        log.info(String.valueOf(checkUser.get().getId()));
+        if (nowUser.isPresent()) {
+            if (nowUser.equals(checkUser)) {
+                return true;
+            } else return false;
+        } else return false;
     }
 
-    public Boolean followState(String email, Long userId) {
-        Optional<User> user = userRepository.findByEmail(email);
+    private Boolean followState(String nowUserEmail, Long userId) {
+        Optional<User> user = userRepository.findByEmail(nowUserEmail);
 
+        /**
+         * 팔로우 하면 true
+         * 아니면 false ( 같은 유저여도 false)
+         */
         if (user.isPresent()) {
             Optional<FollowState> followState = followRepository.findByPushUserAndFollowUser(user.get().getId(), userId);
-            return followState.isEmpty(); // 팔로우 상태가 비어있으면 true
+            return followState.isPresent();
         } else {
-            return true;//팔로우 안한 상태면 false
+            return false;
         }
     }
-
-
 }
-
