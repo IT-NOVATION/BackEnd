5cd4384990071b60113508566867894dc731a406
djc06048 <gsafe1213@gmail.com>
2023-07-24 15:46:32 +0900

    [chore] deploy.sh 수정

--- ItNovation/scripts/deploy.sh
+++ ItNovation/scripts/deploy.sh
@@ -28,6 +28,6 @@ chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+  -Dspring.config.location=optional:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profile.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
