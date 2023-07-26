430c9e4fd777cb6308e7a9ea8ff565da35373161
djc06048 <gsafe1213@gmail.com>
2023-07-27 00:19:59 +0900

    [feat] 댓글알림 서비스 추가

--- ItNovation/src/main/java/com/ItsTime/ItNovation/controller/comment/CommentController.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/comment/CommentController.java
@@ -2,6 +2,10 @@ package com.ItsTime.ItNovation.controller.comment;
 
 
 import com.ItsTime.ItNovation.domain.comment.dto.CommentWriteRequestDto;
+import com.ItsTime.ItNovation.domain.review.ReviewRepository;
+import com.ItsTime.ItNovation.domain.user.User;
+import com.ItsTime.ItNovation.domain.user.UserRepository;
+import com.ItsTime.ItNovation.notify.NotificationService;
 import com.ItsTime.ItNovation.service.comment.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,17 +19,33 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/comment")
 @RequiredArgsConstructor
 @Slf4j
 public class CommentController {
 
+    private final NotificationService notificationService;
+    private final UserRepository userRepository;
+    private final ReviewRepository reviewRepository;
 
     private final CommentService commentService;
     @PostMapping("/write")
     public ResponseEntity commentWrite(@RequestBody CommentWriteRequestDto commentDto, Authentication authentication){
+        //리뷰댓글다는 사람(pub)
         String email = authentication.getName();
+        Optional<User> pubuser = userRepository.findByEmail(email);
+        if(pubuser.isPresent()){
+            Long pubId=pubuser.get().getId();
+            //리뷰주인- 알람받는사람(sub)
+            Long reviewId = commentDto.getReviewId();
+            Long subId=reviewRepository.findById(reviewId).get().getUser().getId();
+            //subId에게 리뷰댓글다는 사람의 id 를 알려주고 이벤트 카테고리는 리뷰댓글
+            //프론트엔드가 이제 subId로 sse 연결을 맺고 pubId님이 댓글을 달았다고 공지 주면됨
+            notificationService.notify(subId,pubId,"comment");
+        }
 
         return commentService.writeComment(commentDto, email);
     }
