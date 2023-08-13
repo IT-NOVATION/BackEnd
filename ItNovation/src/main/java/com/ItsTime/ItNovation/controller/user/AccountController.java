15e38ca0d6e8486623d88267d551b54f91755afc
khyojun <nandong5923@naver.com>
2023-08-13 13:59:21 +0900

    [fix] AccountController 생성 및 api 관련 수정

--- ItNovation/src/main/java/com/ItsTime/ItNovation/controller/user/AccountController.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/user/AccountController.java
@@ -1,9 +1,49 @@
 package com.ItsTime.ItNovation.controller.user;
 
-public class LoginController {
+import com.ItsTime.ItNovation.domain.user.dto.SignUpRequestDto;
+import com.ItsTime.ItNovation.jwt.service.JwtService;
+import com.ItsTime.ItNovation.service.user.UserLoginStateService;
+import com.ItsTime.ItNovation.service.user.UserProfileService;
+import com.ItsTime.ItNovation.service.user.UserService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/api/v1/account")
+public class AccountController {
 
+    private final UserService userService;
+    private final JwtService jwtService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@RequestBody SignUpRequestDto signUpRequestDto) {
+        log.info("회원가입");
+        return userService.join(signUpRequestDto);
+    }
+    @GetMapping("/custom-logout")
+    public ResponseEntity logout(HttpServletRequest request) {
+        log.info("로그아웃");
+        Optional<String> accessToken = jwtService.extractAccessToken(request);
+        if(accessToken.isEmpty()){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("유효하지 않은 토큰입니다.");
+        }else{
+            log.info("엑세스 토큰: {}" ,accessToken.get());
+
+            return userService.logout(accessToken.get());
+        }
+
+    }
 
 
 
