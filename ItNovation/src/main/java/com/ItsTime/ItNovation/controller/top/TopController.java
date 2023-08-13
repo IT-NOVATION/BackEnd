02b0e699c030a811510c39607b73000d6e1dd026
khyojun <nandong5923@naver.com>
2023-08-13 13:59:00 +0900

    [fix] TopController 기능 결합 및 api 관련 수정

--- ItNovation/src/main/java/com/ItsTime/ItNovation/controller/top/TopController.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/top/TopController.java
@@ -1,24 +1,42 @@
 package com.ItsTime.ItNovation.controller.top;
 
 
-import com.ItsTime.ItNovation.domain.user.UserRepository;
+import com.ItsTime.ItNovation.jwt.service.JwtService;
+import com.ItsTime.ItNovation.service.bestUser.TodayBestUserService;
 import com.ItsTime.ItNovation.service.top.TopKeywordService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/top")
+@RestController
 @RequiredArgsConstructor
-public class TopKeywordController {
+@RequestMapping("/api/v1/top")
+public class TopController {
     private final TopKeywordService topKeywordService;
+    private final TodayBestUserService todayBestUserService;
+    private final JwtService jwtService;
 
-    @GetMapping("/moviekeyword/{movieId}")
+    @GetMapping("/movie-keyword/{movieId}")
     public ResponseEntity topKeyword(@PathVariable Long movieId){
         return topKeywordService.findTop3Feature(movieId);
+    }
+
+
+    @GetMapping("/yesterday/user")
+    public ResponseEntity yesterdayTopUser(HttpServletRequest request){
+        Optional<String> s = jwtService.extractAccessToken(request);
+        if(s.isPresent()){
+            Optional<String> email = jwtService.extractEmail(s.get());
+            if(email.isPresent()) {
+                return todayBestUserService.getBestUserInfo(email.get());
+            }
+        }
+        return todayBestUserService.getBestUserInfo(null);
     }
 
 }
