0c2828ea97ce14457e0caca798a3c53ab1986692
khyojun <nandong5923@naver.com>
2023-08-13 13:58:40 +0900

    [fix] SearchController api 관련 수정

--- ItNovation/src/main/java/com/ItsTime/ItNovation/controller/search/SearchController.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/controller/search/SearchController.java
@@ -1,7 +1,12 @@
 package com.ItsTime.ItNovation.controller.search;
 
 import com.ItsTime.ItNovation.domain.movie.dto.MovieSearchResponseDto;
+import com.ItsTime.ItNovation.domain.user.dto.UserSearchTotalResponseDto;
+import com.ItsTime.ItNovation.jwt.service.JwtService;
 import com.ItsTime.ItNovation.service.movie.MovieSearchService;
+import com.ItsTime.ItNovation.service.user.UserSearchService;
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -10,15 +15,24 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RestController
 @Slf4j
-@RequestMapping("search")
-public class MovieSearchController {
+@RequestMapping("/api/v1/search")
+public class SearchController {
     private final MovieSearchService movieSearchService;
+    private final UserSearchService userSearchService;
+    private final JwtService jwtService;
 
     @GetMapping("/movie")
     public ResponseEntity <MovieSearchResponseDto> searchMoviesByTitle(@RequestParam(name="movieNm") String movieNM) {
         return movieSearchService.searchMoviesByTitle(movieNM);
 
     }
+
+    @GetMapping("/user")
+    public ResponseEntity <UserSearchTotalResponseDto> searchTargetUser(@RequestParam(name="userNm") String userName, HttpServletRequest request){
+        Optional<String> accessToken = jwtService.extractAccessToken(request);
+        return userSearchService.getTotalResponse(userName,accessToken);
+    }
+
 }
 
 
