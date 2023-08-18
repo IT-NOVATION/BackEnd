d360aeaa62e5ecebe592390ba4f479a0922cc3dd
djc06048 <gsafe1213@gmail.com>
2023-08-18 23:58:21 +0900

    [fix] 소셜로그인 안되는 문제 해결 스프링시큐리티 관련 에러

--- ItNovation/src/main/java/com/ItsTime/ItNovation/config/jwt/filter/JwtExceptionFilter.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/config/jwt/filter/JwtExceptionFilter.java
@@ -5,15 +5,18 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
+@Slf4j
 public class JwtExceptionFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.info("JwtExceptionFilter: Handling JWT exceptions...");
         try {
             filterChain.doFilter(request, response); // JwtAuthenticationFilter로 이동
         } catch (UnauthorizedException ex) {
