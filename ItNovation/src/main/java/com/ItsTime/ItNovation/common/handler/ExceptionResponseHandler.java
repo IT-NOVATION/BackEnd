8aead857185c79d667f9ff7faf6cc24861895200
djc06048 <gsafe1213@gmail.com>
2023-08-23 01:03:44 +0900

    [refactor] 에러 응답 방식 변경

--- ItNovation/src/main/java/com/ItsTime/ItNovation/common/handler/ExceptionResponseHandler.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/common/handler/ExceptionResponseHandler.java
@@ -1,8 +1,7 @@
 package com.ItsTime.ItNovation.common.handler;
 
-import com.ItsTime.ItNovation.common.exception.ErrorBaseResponse;
-import com.ItsTime.ItNovation.common.exception.ErrorCode;
-import com.ItsTime.ItNovation.common.exception.UnauthorizedException;
+import com.ItsTime.ItNovation.common.exception.*;
+import org.aspectj.weaver.ast.Not;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
@@ -12,10 +11,27 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class ExceptionResponseHandler {
 
-    @ExceptionHandler(UnauthorizedException.class)
-    public ResponseEntity<String> handleJwtException(UnauthorizedException e){
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.getMessage());
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity handleForbiddenException(BadRequestException e){
+        final ErrorBaseResponse errorBaseResponse = ErrorBaseResponse.of(e.getErrorCode());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorBaseResponse);
     }
+    @ExceptionHandler(UnauthorizedException.class)
+    public ResponseEntity handleJwtException(UnauthorizedException e){
+        final ErrorBaseResponse errorBaseResponse = ErrorBaseResponse.of(e.getErrorCode());
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorBaseResponse);
+    }
+    @ExceptionHandler(ForbiddenException.class)
+    public ResponseEntity handleForbiddenException(ForbiddenException e){
+        final ErrorBaseResponse errorBaseResponse = ErrorBaseResponse.of(e.getErrorCode());
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(errorBaseResponse);
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity handleForbiddenException(NotFoundException e){
+        final ErrorBaseResponse errorBaseResponse = ErrorBaseResponse.of(e.getErrorCode());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorBaseResponse);
+    }
+
 
     /**
      * 지원하지 않는 HTTP method로 요청 시 발생하는 error를 handling합니다.
