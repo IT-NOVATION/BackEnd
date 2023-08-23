4f81e7ebb624faf4c0749e2d99e25bcd5cceaeaa
khyojun <nandong5923@naver.com>
2023-08-24 00:19:06 +0900

    [refactor] email 관련 비즈니스 로직 리팩토링 작업

--- ItNovation/src/main/java/com/ItsTime/ItNovation/service/mail/EmailService.java
+++ ItNovation/src/main/java/com/ItsTime/ItNovation/service/mail/EmailService.java
@@ -20,6 +20,10 @@ import org.thymeleaf.spring6.SpringTemplateEngine;
 @Slf4j
 public class EmailService {
 
+    @Value("${spring.mail.username}")
+    private final String from;
+    private final String title = "MOVIELOG 비밀번호 교체 관련 인증 번호";
+
     private final JavaMailSender javaMailSender;
 
     private final SpringTemplateEngine templateEngine;
@@ -27,63 +31,50 @@ public class EmailService {
     private String authNum;
 
 
-    @Value("${spring.mail.username}")
-    private String from;
-
-
-    public String sendMail(String toEmail) throws MessagingException, UnsupportedEncodingException{
-        log.info("senemail service");
+    public String sendMail(String toEmail) throws MessagingException, UnsupportedEncodingException {
         MimeMessage emailForm = createEmailForm(toEmail);
         javaMailSender.send(emailForm);
-        log.info("javamailsender");
         return authNum;
     }
-    public MimeMessage createEmailForm(String email) throws MessagingException, UnsupportedEncodingException{
+
+    private MimeMessage createEmailForm(String email)
+        throws MessagingException {
         createCode();
         String toEmail = email;
-        String title = "MOVIELOG 비밀번호 교체 관련 인증 번호";
+        MimeMessage message = setMimeMessage(toEmail);
+        return message;
+    }
 
+    private MimeMessage setMimeMessage(String toEmail) throws MessagingException {
         MimeMessage message = javaMailSender.createMimeMessage(); //MimeMessage 객체를 직접 생성하여 메일을 발송하는 방법
-
         message.addRecipients(RecipientType.TO, toEmail);
         message.setSubject(title);
         message.setFrom(from);
         message.setText(setContext(authNum), "utf-8", "html");
-        log.info(email);
         return message;
     }
 
-    public void createCode() { // 인증번호 무작위 6자리 -> 문자로 들어가게 됨.
-
+    private void createCode() { // 인증번호 무작위 6자리 -> 문자로 들어가게 됨.
         Random random = new Random();
-
         StringBuffer key = new StringBuffer();
-
         for (int i = 0; i < 6; i++) {
             int index = random.nextInt(9);
-            key.append(Integer.toString(index));
+            key.append(index);
             authNum = key.toString();
         }
-        log.info(authNum);
     }
 
 
-
-
-
-
-
-    public Boolean isCodeSame(String sendCode, String email, Map<String, String> authCodeMap){
+    public Boolean isCodeSame(String sendCode, String email, Map<String, String> authCodeMap) {
         String rightCode = authCodeMap.get(email);
         return sendCode.equals(rightCode);
     }
 
 
-    //타임리프를 이용한 context 설정
-    public String setContext(String code) {
-        Context context = new Context(); // thymeleaf 사용을 위해 사용
+    private String setContext(String code) {
+        Context context = new Context();
         context.setVariable("code", code);
-        return templateEngine.process("mailTest", context); //mail.html
+        return templateEngine.process("mailTest", context);
     }
 
 
